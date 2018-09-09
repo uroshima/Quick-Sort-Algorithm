685f3e69cdca98b38cbbad07ebdde5ab7655ca7a
urosh <urosh.stojkovikj@gmail.com>
2018-09-08 21:32:13 -0700

    complete

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -21,27 +21,30 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    debugger
-    return array if length <= 1
-    prc ||= Proc.new { |a, b| a <=> b }
-    pivot = array[0]
-    i = 1
-    start = 1
-    while i < length
-      if prc.call(array[i], pivot) == -1
-        if array[start...i].empty? == false || array[start...i].first != array[i]
-          array[i], array[start] = array[start], array[i]
-        end
-        start += 1
-      end
-      i += 1
-    end
-    array[0], array[start - 1] = array[start - 1], array[0]
-    p array
-    QuickSort.sort2!(array[0...start - 1], start = 0, length = array[0...start - 1].length, &prc) + [pivot] + QuickSort.sort2!(array[start...length], start, length = array[start...length].length, &prc)
-  end
+     prc ||= Proc.new { |a,b| a <=> b }
+     return array if length < 2
+     pivot_idx = QuickSort.partition(array, start, length, &prc)
+     left_len = pivot_idx - start
+     right_len = length - (left_len + 1)
+     QuickSort.sort2!(array, start, left_len, &prc)
+     QuickSort.sort2!(array, pivot_idx + 1,right_len, &prc)
+     return array
+   end
 
-  def self.partition(array, start, length, &prc)
-
+   def self.partition(array, start, length, &prc)
+     prc ||= Proc.new { |a,b| a <=> b }
+     pivot_idx = start
+     pivot = array[start]
+     ((start+1)..(start + length -1)).each do |idx|
+       # when new el is less than pivot el
+       if prc.call(array[idx], pivot) < 0
+           temp = array[pivot_idx+1]
+           array[pivot_idx+1] = array[idx]
+          array[idx] = temp
+          pivot_idx += 1
+       end
+     end
+     array[start], array[pivot_idx] = array[pivot_idx], array[start]
+     pivot_idx
+   end
   end
-end
